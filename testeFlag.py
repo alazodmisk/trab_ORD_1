0970ccf13054dd693571fd592caa6e4bca67887b
Guilherme H. V. Pichitelli Vitor <pichitellig@gmail.com>
2026-04-22 16:26:37 -0300

    Update testeFlag.py

--- testeFlag.py
+++ testeFlag.py
@@ -3,11 +3,29 @@ import sys
 def construir_indices():
     print("Iniciando a construção dos índices...")
 
+    with open("publicadora.ind", "w+") as publicadora:
+        publicadora.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
+
+    with open("genero.ind", "w+") as genero:
+        genero.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
+
+    with open("listaInvertida.lst", "w+") as listaInvertida:
+        listaInvertida.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
+
+    with open("primario.ind", "w+") as primario:
+        primario.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
+
+
+
 def executar_operacoes(nome_arquivo):
     print(f"Executando operações do arquivo: {nome_arquivo}")
 
+
+
 def compactar_arquivo():
     print("Iniciando a compactação do arquivo...")
+
+
 
 def main():
     if len(sys.argv) < 2:
