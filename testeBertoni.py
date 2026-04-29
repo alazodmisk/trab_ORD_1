112136c45360a2c127ae0bc99b8bc43ea91e056f
PedroHBertoni <phbertoni290307ph@gmail.com>
2026-04-29 13:35:31 -0300

    Nao tendi pq ta dando '[' incompletado

--- testeBertoni.py
+++ testeBertoni.py
@@ -23,14 +23,14 @@ class genero:
 
 class indicesInvertida:
     indice: int
-    proxPublicadora: int
     proxGenero: int
+    proxPublicadora: int
 
 
 
 
 #FUNÇÕES AUXILIARES
-def busca_binaria(num:int, lista:list[indicesInvertida]) -> indicesInvertida:
+def busca_binaria(num:int, lista:list[indicesInvertida] | list[identificador]) -> indicesInvertida | identificador | None:
     inicio = 0
     final = len(lista)
     while inicio <= final:
@@ -41,8 +41,7 @@ def busca_binaria(num:int, lista:list[indicesInvertida]) -> indicesInvertida:
             inicio = media + 1
         else:
             final = media - 1
-
-
+    return None
 
 
 
@@ -54,12 +53,11 @@ def construir_indices():
 
     with open("games.dat", "rb") as games:
         buffer = games.read(2)
-        while buffer != "+":
+        while buffer != "":
             tam = int.from_bytes(buffer)
-            buffer = games.read(buffer)
-            buffer = buffer.decode()
+            buffer = games.read(tam).decode()
 
-            registro:list = buffer.split("|") #[ID, Nome, Ano, Genero, Publicadora, Plataforma]
+            registro:list[str] = buffer.split("|") #[ID, Nome, Ano, Genero, Publicadora, Plataforma]
 
             indice = int(registro[0])
             primarioLista = [identificador(indice, tam)] + primarioLista
@@ -71,15 +69,8 @@ def construir_indices():
                 else:
                     primarioLista[i+1].offset += tam
 
-            #INCLUSAO ORDENADA NA LISTA INVERTIDA
+            #INCLUSAO COMO ELEMENTO 0 NA LISTA INVERTIDA
             listaInvertida = [indicesInvertida(indice, -1, -1)] + listaInvertida
-            for i in range(len(listaInvertida)):
-                if listaInvertida[i].indice > listaInvertida[i+1].indice:
-                    aux = listaInvertida[i]
-                    listaInvertida[i] = listaInvertida[i+1]
-                    listaInvertida[i+1] = aux
-                else:
-                    break
 
             #ATUALIZAÇAO DE INDICES EM RELACAO A GENERO
             jaPresente = False
@@ -91,9 +82,10 @@ def construir_indices():
                     break
 
             if jaPresente:
+                indice = busca_binaria(indice_aux, listaInvertida).proxGenero
                 while indice != -1:
-                    indice = busca_binaria(indice_aux, listaInvertida).proxGenero
-                listaInvertida[indice].proxGenero = indice
+                    indice = busca_binaria(indice, listaInvertida).proxGenero
+                listaInvertida[indice].proxGenero = listaInvertida[0].indice
             else:
                 generoLista += [genero(aux, indice)]
 
@@ -104,54 +96,50 @@ def construir_indices():
                 if i.nome == aux:
                     jaPresente = True
                     indice_aux = i.indice
-                    break
+                    break 
 
             if jaPresente:
+                indice = busca_binaria(indice_aux, listaInvertida).proxPublicadora
                 while indice != -1:
-                    indice = busca_binaria(indice_aux, listaInvertida).proxPublicadora
-                listaInvertida[indice].proxPublicadora = indice
+                    indice = busca_binaria(indice, listaInvertida).proxPublicadora
+                listaInvertida[indice].proxPublicadora = listaInvertida[0].indice
             else:
                 publicadoraLista += [publicadora(aux, indice)]
-            
-                
-                    
 
 
+        #ORDENACAO DA LISTA INVERTIDA
+        for i in range(len(listaInvertida)):
+            if listaInvertida[i].indice > listaInvertida[i+1].indice:
+                aux = listaInvertida[i]
+                listaInvertida[i] = listaInvertida[i+1]
+                listaInvertida[i+1] = aux
+            else:
+                break
 
-    print("Iniciando a construção dos índices...")
-    open(games.dat)
-    buffer = games.read(1)
-    
+
+        buffer = games.read(2)
+
     with open("primario.ind", "w+") as primario:
-        primario.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
-        primario.ind[anterior], primario.ind[tamanho anterior]+buffer
-
-    buffer = games.read(buffer)
-
-
+        for i in primarioLista:
+            linha = str(i.indice) + " " + str(i.offset)
+            primario.write(linha + "\n") 
 
     with open("publicadora.ind", "w+") as publicadora:
-        
-        publicadora.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
-        buffer2 = buffer[x,y] 
-        for i in publicadora.ind:
-            se achar -> pula
-            se nao -> adiciona o buffer2 com seu indice primario
-
-
+        for i in publicadoraLista:
+            linha = i.nome + " " + str(i.indice)
+            primario.write(linha + "\n") 
+            
     with open("genero.ind", "w+") as genero:
-        genero.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
-        buffer2 = buffer[x,y] 
-        for i in genero.ind:
-            se achar -> pula
-            se nao -> adiciona o buffer2 com seu indice primario
+        for i in generoLista:
+            linha = i.nome + " " + str(i.indice)
+            primario.write(linha + "\n") 
 
     with open("listaInvertida.lst", "w+") as listaInvertida:
-        listaInvertida.write("Conteúdo do arquivo de índice\n") ## Aqui vai uma iteração para ir adicionando as infos
+        for i in listaInvertida:
+            linha = str(i.indice) + " " + str(i.proxGenero) + " " + str(i.proxPublicadora)
+            primario.write(linha + "\n") 
 
     
-
-
 
 def executar_operacoes(nome_arquivo):
     print(f"Executando operações do arquivo: {nome_arquivo}")
@@ -174,26 +162,16 @@ def main():
     if flag == '-b':
         construir_indices()
 
-
-
-
-
     elif flag == '-e':
         if len(sys.argv) < 3:
             print("Erro: Para usar -e, você deve informar o arquivo de operações.")
         else:
             arquivo_ops = sys.argv[2]
             executar_operacoes(arquivo_ops)
-            
-
-
 
     elif flag == '-c':
         compactar_arquivo()
         
-
-
-
     else:
         print(f"Flag '{flag}' não reconhecida.")
 
