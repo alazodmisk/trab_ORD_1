3f7dd796a6db262b7844d7d53432362d91d71ef5
PedroHBertoni <phbertoni290307ph@gmail.com>
2026-04-30 16:33:24 -0300

    Update testeBertoni.py

--- testeBertoni.py
+++ testeBertoni.py
@@ -33,13 +33,15 @@ class IndicesInvertida:
 
 #FUNÇÕES AUXILIARES
 def busca_binaria(num:int, lista:list[IndicesInvertida] | list[Identificador]) -> IndicesInvertida | Identificador | None:
-    inicio = 0
+    inicio = 1
     final = len(lista) - 1
+    print(num)
     while inicio <= final:
         media = (inicio+final)//2
+        print(f"{inicio} + {final} = {media} < {len(lista)}")
         if lista[media].indice == num:
             return lista[media]
-        elif lista[media].indice >= num:
+        elif lista[media].indice > num:
             inicio = media + 1
         else:
             final = media - 1
@@ -50,7 +52,7 @@ def nome_presente(nome: str, lista: list[Genero] | list[Publicadora]) -> int:
     for i in lista:
         if i.nome == nome:
             return i.indice
-    return -2
+    return -1
 
 def troca(i: int, lista: list[Identificador|IndicesInvertida]) -> None:
     aux = lista[i+1]
@@ -84,31 +86,39 @@ def construir_indices():
 
 
             #INCLUSAO COMO ELEMENTO 0 NA LISTA INVERTIDA
-            listaInvertida = [IndicesInvertida(indice, -1, -1)] + listaInvertida
+            listaInvertida = listaInvertida + [IndicesInvertida(indice, -1, -1)]
 
 
             #ATUALIZAÇAO DE INDICES EM RELACAO A GENERO
-            indiceProx = nome_presente(registro[3], generoLista)
+            indiceGen = nome_presente(registro[3], generoLista) #Primeira aparição do Gênero entre os jogos
+            print(f"Primeiro: {indiceGen}")
 
-            while indiceProx > -1:
-                indiceProx = busca_binaria(indice, listaInvertida).proxGenero
-            if indiceProx == -1: #Último caso do Gênero
-                listaInvertida[indice].proxGenero = listaInvertida[0].indice
-            else: #indiceProx == -2 -> Primeiro caso do Gênero
+            if indiceGen != -1: # Ir para o último caso do Gênero
+                indiceGen = busca_binaria(indiceGen, listaInvertida).proxGenero
+                while listaInvertida[indiceGen].proxGenero > -1:
+                    print(indiceGen)
+                    indiceGen = busca_binaria(indiceGen, listaInvertida).proxGenero
+                listaInvertida[indiceGen].proxGenero = indice
+            else: #Gênero nunca antes registrado
                 generoLista = generoLista + [Genero(registro[3], indice)]
 
 
             #ATUALIZAÇAO DE INDICES EM RELACAO A PUBLICADORA
-            indiceProx = nome_presente(registro[4], publicadoraLista)
+            indicePubl = nome_presente(registro[4], publicadoraLista) #Primeira aparição da Publicadora entre os jogos
+            print(f"Primeira: {indicePubl}")
 
-            while indiceProx > -1:
-                indiceProx = busca_binaria(indiceProx, listaInvertida).proxPublicadora
-            if indiceProx == -1: #Último caso da Publicadora 
-                listaInvertida[indiceProx].proxPublicadora = listaInvertida[0].indice
-            else: #indiceProx == -2 -> Primeiro caso da Publicadora
+            if indicePubl != -1: #Ir para o último caso da Publicadora 
+                indicePubl = busca_binaria(indicePubl, listaInvertida).proxPublicadora
+                while listaInvertida[indicePubl].proxPublicadora > -1:
+                    print(indicePubl)
+                    indicePubl = busca_binaria(indicePubl, listaInvertida).proxPublicadora
+                listaInvertida[indicePubl].proxPublicadora = indice
+            else: #Publicadora nunca antes registrada
                 publicadoraLista = publicadoraLista + [Publicadora(registro[4], indice)]
+            
 
-
+            print(f"{listaInvertida[0].proxGenero} and {listaInvertida[0].proxPublicadora}")
+            print()
             #ORDENACAO DA LISTA INVERTIDA
             for i in range(len(listaInvertida)-1):
                 if listaInvertida[i].indice > listaInvertida[i+1].indice:
